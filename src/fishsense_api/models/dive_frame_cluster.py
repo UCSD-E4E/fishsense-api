cc80c4f0872925b31dde38ee0cff9e3491ab58b5
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-11-16 22:37:10 -0800

    feat: add data frame cluster datasource

--- src/fishsense_api/models/dive_frame_cluster.py
+++ src/fishsense_api/models/dive_frame_cluster.py
@@ -1,15 +1,20 @@
 """Models for Dive Frame Clusters in FishSense API."""
 
+from datetime import datetime
 from typing import List
 
 from pydantic import BaseModel
-from sqlmodel import Field, SQLModel
+from sqlmodel import Column, DateTime, Enum, Field, SQLModel
+
+from fishsense_api.models.data_source import DataSource
 
 
 class DiveFrameCluster(SQLModel, table=True):
     """Model representing a cluster of frames within a dive."""
 
     id: int | None = Field(default=None, primary_key=True)
+    data_source: DataSource = Field(sa_column=Column(Enum(DataSource)))
+    updated_at: datetime | None = Field(sa_type=DateTime(timezone=True), default=None)
 
     dive_id: int | None = Field(default=None, foreign_key="dive.id")
 
